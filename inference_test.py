fd8f7da9a87fba54e5938961d7375047e89ff81f
ZhangAoCanada <zhangaocanada@gmail.com>
2022-02-25 15:58:06 +0800

    start debugging

--- inference_test.py
+++ inference_test.py
@@ -29,9 +29,11 @@ import cv2, time
 
 # ----------------- from TransWeather ------------------
 def calc_psnr(im1, im2):
-    im1, im2 = im1 * 255, im2 * 255
-    im1, im2 = im1.astype(np.int8), im2.astype(np.int8)
     print("-----------------------")
+    print(im1.shape, im1.max(), im1.min())
+    print(im2.shape, im2.max(), im2.min())
+    im1, im2 = im1 * 255.0, im2 * 255.0
+    im1, im2 = im1.astype(np.int8), im2.astype(np.int8)
     print(im1.shape, im1.max(), im1.min())
     print(im2.shape, im2.max(), im2.min())
     im1_y = cv2.cvtColor(im1, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
@@ -40,7 +42,7 @@ def calc_psnr(im1, im2):
     return ans
 
 def calc_ssim(im1, im2):
-    im1, im2 = im1 * 255, im2 * 255
+    im1, im2 = im1 * 255.0, im2 * 255.0
     im1, im2 = im1.astype(np.int8), im2.astype(np.int8)
     im1_y = cv2.cvtColor(im1, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
     im2_y = cv2.cvtColor(im2, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
