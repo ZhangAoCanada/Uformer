f6da875e6db00e8647a278326d62d724e8e10c30
ZhangAoCanada <zhangaocanada@gmail.com>
2022-02-25 15:51:14 +0800

    start debugging inference

--- inference_test.py
+++ inference_test.py
@@ -32,8 +32,8 @@ def calc_psnr(im1, im2):
     im1, im2 = im1 * 255, im2 * 255
     im1, im2 = im1.round().astype(np.int8), im2.round().astype(np.int8)
     print("-----------------------")
-    print(im1.shape)
-    print(im2.shape)
+    print(im1.shape, im1.max(), im1.min())
+    print(im2.shape, im2.max(), im2.min())
     im1_y = cv2.cvtColor(im1, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
     im2_y = cv2.cvtColor(im2, cv2.COLOR_BGR2YCR_CB)[:, :, 0]
     ans = [compare_psnr(im1_y, im2_y)]
